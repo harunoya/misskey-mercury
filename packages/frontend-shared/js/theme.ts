56ea11f62b495c86e2b3b3787f203a066e54cb30
caipira113 <caipira@libnare.net>
2023-07-08 19:08:47 +0900

    buttersc.one theme
    (cherry picked from commit 302c1db19d111cf08be47925366250c0da215851)

--- packages/frontend-shared/js/theme.ts
+++ packages/frontend-shared/js/theme.ts
@@ -36,6 +36,7 @@ export const themeProps = Object.keys(lightTheme.props).filter(key => !key.start
 export const getBuiltinThemes = () => Promise.all(
 	[
 		'l-birdsite',
+		'l-scone-color',
 		'l-light',
 		'l-coffee',
 		'l-apricot',
@@ -53,6 +54,7 @@ export const getBuiltinThemes = () => Promise.all(
 		'l-u0',
 
 		'd-birdsite',
+		'd-scone-color',
 		'd-dark',
 		'd-qdon',
 		'd-persimmon',
