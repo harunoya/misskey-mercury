fbedf09581b8370ab7b157ea477a1d8fd84b5c3b
syuilo <4439005+syuilo@users.noreply.github.com>
2026-08-03 06:10:20 +0900

    Update engine.ts

--- packages/frontend-misskey-world-engine/src/engine.ts
+++ packages/frontend-misskey-world-engine/src/engine.ts
@@ -78,7 +78,7 @@ export class WorldEngine extends MultiplayEngineBase<{
 		this.celShadingRenderer = new CelShadingRenderer(this.scene, {
 			enabled: true,
 			color: new BABYLON.Color3(0.4, 0.5, 0.6),
-			width: cm(1),
+			width: cm(2),
 		});
 		this.sr = new BABYLON.SnapshotRenderingHelper(this.scene);
 
